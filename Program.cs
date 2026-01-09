75ebf7985f605c8301440665e3e3497d22c69565
Aryan Mishra <me@aryanmishra.com>
2026-01-09 19:21:20 +0530

    using static files middleware to add bootstrap through layout

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// MVC with runtime if in development
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services
+        .AddControllersWithViews()
+        .AddRazorRuntimeCompilation();
+}
+else
+{
+    builder.Services.AddControllersWithViews();
+}
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+// ✅ Standard MVC routing ONLY
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
